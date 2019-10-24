f5b3ea9378c4f2eda1ceb760931a84ab65256aad
Mihail <oranze@mail.ru>
2019-10-24 11:00:57 +0200

    Added RabbitMQ test queue's removing in start of tests

--- rabbitmqLowLvlHandler_test.go
+++ rabbitmqLowLvlHandler_test.go
@@ -10,6 +10,51 @@ import (
 )
 
 func TestMain( m *testing.M ){
+    // Clean old queue's
+    var (
+        err     error
+        i       int64   = 0
+        conn    *amqp.Connection
+        channel *amqp.Channel
+        queue   string
+    )
+
+    err = Init( "unit", TEST_HOST_OK, "test", "test", TEST_PORT_OK, 5 )
+    if err != nil{
+        fmt.Println( "Can't init handler" )
+        os.Exit( 999 )
+    }
+
+    conn, err = GetNewConnection()
+    if err == nil{
+
+        channel, err = GetNewChannel( conn )
+        if err == nil{
+            Settings.i = 20
+            for i <= Settings.i{
+                queue = QUEUE_NAME + fmt.Sprintf( "%d", i )
+                str :=  "Try to remove QUEUE: "+queue+" ..."
+                err = DeleteQueue( channel, &queue )
+                if err == nil{
+                    str = str +" OK"
+                    fmt.Println( str )
+                } else{
+                    str = str +" Fail. error: %v"
+                    fmt.Println( fmt.Sprintf( str, err ) )
+                }
+                i++
+            }
+            Settings.i = 0
+        } else{
+            fmt.Println( fmt.Sprintf( "Can't remove QUEUE'S. Channel problem. Err: %v", err ) )
+        }
+
+
+    } else{
+        fmt.Println( fmt.Sprintf( "Can't remove QUEUE'S. Connector problem. Err: %v", err ) )
+    }
+
+    // start testing
     exitCode := m.Run()
     if exitCode == 0 {
         err := logger.DeleteLogFolder()
